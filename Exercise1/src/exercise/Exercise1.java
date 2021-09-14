e1fa5b3a46f141372e80b49c9a9cd5ab42c0068f
dung1308 <90315963+dung1308@users.noreply.github.com>
2021-09-14 07:56:30 +0700

    Add files via upload

---
+++ Exercise1/src/exercise/Exercise1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+ 		int number1 = 125;
+ 		int number2 = 2019;
+ 		System.out.println("Sum: " + (number1 + number2));
+    }
+}
